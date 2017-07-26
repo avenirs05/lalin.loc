6be7f073a9f827f2af71d9fbb91661db05381e43
avenirs05 <avenirs05@gmail.com>
2017-07-26 18:07:19 +0300

    last

--- page-services.php
+++ page-services.php
@@ -7,12 +7,12 @@
         	<div class="category-service-wrap">
         		<h4 class="head-category-service-wrap text-center">Инъекционная косметология</h4>
         		<ul>        			
-					<a href=""><li>Биоревитализация</li></a>
-					<a href=""><li>Ботокс, Диспорт</li></a>
-					<a href=""><li>Изменение формы и объема губ</li></a>
-					<a href=""><li>Контурная пластика</li></a>
-					<a href=""><li>Мезотерапия</li></a>
-					<a href=""><li>Плазмолифтинг</li></a>
+					<a href="" target="_blank"><li>Биоревитализация</li></a>
+					<a href="" target="_blank"><li>Ботокс, Диспорт</li></a>
+					<a href="" target="_blank"><li>Изменение формы и объема губ</li></a>
+					<a href="" target="_blank"><li>Контурная пластика</li></a>
+					<a href="" target="_blank"><li>Мезотерапия</li></a>
+					<a href="" target="_blank"><li>Плазмолифтинг</li></a>
         		</ul>
         	</div>
         </div>
@@ -20,11 +20,11 @@
         	<div class="category-service-wrap">
         		<h4 class="head-category-service-wrap text-center">Коррекция фигуры, целлюлит</h4>
 				<ul>					
-					<a href=""><li>Комплексные программы по снижению веса</li></a>
-					<a href=""><li>Прессотерапия</li></a>
-					<a href=""><li>УЗ-липосакция с RF-лифтингом</li></a>
-					<a href=""><li>Массаж</li></a>
-					<a href=""><li>Иглорефлексотерапия</li></a>
+					<a href="" target="_blank"><li>Комплексные программы по снижению веса</li></a>
+					<a href="" target="_blank"><li>Прессотерапия</li></a>
+					<a href="" target="_blank"><li>УЗ-липосакция с RF-лифтингом</li></a>
+					<a href="" target="_blank"><li>Массаж</li></a>
+					<a href="" target="_blank"><li>Иглорефлексотерапия</li></a>
 				</ul>
         	</div>
         </div>
@@ -32,13 +32,13 @@
         	<div class="category-service-wrap">
         		<h4 class="head-category-service-wrap text-center">Лазерная косметология</h4>
 				<ul>					
-					<a href=""><li>Глубокое лазерное омоложение с повреждением кожи</li></a>
-					<a href=""><li>Глубокое лазерное омоложение без повреждения кожи</li></a>
-					<a href=""><li>Лечение акне, постакне, розацеа, демодекоза</li></a>
-					<a href=""><li>Фотоомоложение</li></a>
-					<a href=""><li>Удаление рубцов и растяжек</li></a>
-					<a href=""><li>Удаление сосудов на лице и теле</li></a>
-					<a href=""><li>Удаление пигментных пятен</li></a>
+					<a href="" target="_blank"><li>Глубокое лазерное омоложение с повреждением кожи</li></a>
+					<a href="" target="_blank"><li>Глубокое лазерное омоложение без повреждения кожи</li></a>
+					<a href="" target="_blank"><li>Лечение акне, постакне, розацеа, демодекоза</li></a>
+					<a href="" target="_blank"><li>Фотоомоложение</li></a>
+					<a href="" target="_blank"><li>Удаление рубцов и растяжек</li></a>
+					<a href="" target="_blank"><li>Удаление сосудов на лице и теле</li></a>
+					<a href="" target="_blank"><li>Удаление пигментных пятен</li></a>
 				</ul>
         	</div>
         </div>
@@ -48,8 +48,8 @@
         	<div class="category-service-wrap">
         		<h4 class="head-category-service-wrap text-center">Удаление новообразований кожи</h4>
 				<ul>					
-					<a href=""><li>Криодеструкция</li></a>
-					<a href=""><li>Лазерное удаление</li></a>
+					<a href="" target="_blank"><li>Криодеструкция</li></a>
+					<a href="" target="_blank"><li>Лазерное удаление</li></a>
 				</ul>
         	</div>
         </div>
@@ -57,8 +57,8 @@
         	<div class="category-service-wrap">
         		<h4 class="head-category-service-wrap text-center">Эпиляция</h4>
 				<ul>					
-					<a href=""><li>Лазерная эпиляция</li></a>
-					<a href=""><li>Фотоэпиляция</li></a>
+					<a href="" target="_blank"><li>Лазерная эпиляция</li></a>
+					<a href="" target="_blank"><li>Фотоэпиляция</li></a>
 				</ul>
         	</div>
         </div>
@@ -66,10 +66,10 @@
         	<div class="category-service-wrap">
         		<h4 class="head-category-service-wrap text-center">Эстетическая косметология</h4>
 				<ul>
-					<a href=""><li>Криотерапия</li></a>
-					<a href=""><li>Косметический уход</li></a>
-					<a href=""><li>Контурная интимная пластика</li></a>
-					<a href=""><li>Плазмолифтинг в гинекологии</li></a>
+					<a href="" target="_blank"><li>Криотерапия</li></a>
+					<a href="" target="_blank"><li>Косметический уход</li></a>
+					<a href="" target="_blank"><li>Контурная интимная пластика</li></a>
+					<a href="" target="_blank"><li>Плазмолифтинг в гинекологии</li></a>
 				</ul>
         	</div>
         </div>
@@ -80,57 +80,57 @@
 	<div class="category-service-wrap">
 		<h4 class="head-category-service-wrap text-center">Инъекционная косметология</h4>
 		<ul>        			
-			<a href=""><li>Биоревитализация</li></a>
-			<a href=""><li>Ботокс, Диспорт</li></a>
-			<a href=""><li>Изменение формы и объема губ</li></a>
-			<a href=""><li>Контурная пластика</li></a>
-			<a href=""><li>Мезотерапия</li></a>
-			<a href=""><li>Плазмолифтинг</li></a>
+			<a href="" target="_blank"><li>Биоревитализация</li></a>
+			<a href="" target="_blank"><li>Ботокс, Диспорт</li></a>
+			<a href="" target="_blank"><li>Изменение формы и объема губ</li></a>
+			<a href="" target="_blank"><li>Контурная пластика</li></a>
+			<a href="" target="_blank"><li>Мезотерапия</li></a>
+			<a href="" target="_blank"><li>Плазмолифтинг</li></a>
 		</ul>
 	</div>
 	<div class="category-service-wrap">
 		<h4 class="head-category-service-wrap text-center">Коррекция фигуры, целлюлит</h4>
 		<ul>        			
-			<a href=""><li>Комплексные программы по снижению веса</li></a>
-			<a href=""><li>Прессотерапия</li></a>
-			<a href=""><li>УЗ-липосакция с RF-лифтингом</li></a>
-			<a href=""><li>Массаж</li></a>
-			<a href=""><li>Иглорефлексотерапия</li></a>
+			<a href="" target="_blank"><li>Комплексные программы по снижению веса</li></a>
+			<a href="" target="_blank"><li>Прессотерапия</li></a>
+			<a href="" target="_blank"><li>УЗ-липосакция с RF-лифтингом</li></a>
+			<a href="" target="_blank"><li>Массаж</li></a>
+			<a href="" target="_blank"><li>Иглорефлексотерапия</li></a>
 		</ul>
 	</div>
 	<div class="category-service-wrap">
 		<h4 class="head-category-service-wrap text-center">Лазерная косметология</h4>
 		<ul>					
-			<a href=""><li>Глубокое лазерное омоложение с повреждением кожи</li></a>
-			<a href=""><li>Глубокое лазерное омоложение без повреждения кожи</li></a>
-			<a href=""><li>Лечение акне, постакне, розацеа, демодекоза</li></a>
-			<a href=""><li>Фотоомоложение</li></a>
-			<a href=""><li>Удаление рубцов и растяжек</li></a>
-			<a href=""><li>Удаление сосудов на лице и теле</li></a>
-			<a href=""><li>Удаление пигментных пятен</li></a>
+			<a href="" target="_blank"><li>Глубокое лазерное омоложение с повреждением кожи</li></a>
+			<a href="" target="_blank"><li>Глубокое лазерное омоложение без повреждения кожи</li></a>
+			<a href="" target="_blank"><li>Лечение акне, постакне, розацеа, демодекоза</li></a>
+			<a href="" target="_blank"><li>Фотоомоложение</li></a>
+			<a href="" target="_blank"><li>Удаление рубцов и растяжек</li></a>
+			<a href="" target="_blank"><li>Удаление сосудов на лице и теле</li></a>
+			<a href="" target="_blank"><li>Удаление пигментных пятен</li></a>
 		</ul>
 	</div>
 	<div class="category-service-wrap">
 		<h4 class="head-category-service-wrap text-center">Удаление новообразований кожи</h4>
 		<ul>					
-			<a href=""><li>Криодеструкция</li></a>
-			<a href=""><li>Лазерное удаление</li></a>
+			<a href="" target="_blank"><li>Криодеструкция</li></a>
+			<a href="" target="_blank"><li>Лазерное удаление</li></a>
 		</ul>
 	</div>
 	<div class="category-service-wrap">
 		<h4 class="head-category-service-wrap text-center">Эпиляция</h4>
 		<ul>					
-			<a href=""><li>Лазерная эпиляция</li></a>
-			<a href=""><li>Фотоэпиляция</li></a>
+			<a href="" target="_blank"><li>Лазерная эпиляция</li></a>
+			<a href="" target="_blank"><li>Фотоэпиляция</li></a>
 		</ul>
 	</div>
 	<div class="category-service-wrap">
 		<h4 class="head-category-service-wrap text-center">Эстетическая косметология</h4>
 		<ul>
-			<a href=""><li>Криотерапия</li></a>
-			<a href=""><li>Косметический уход</li></a>
-			<a href=""><li>Контурная интимная пластика</li></a>
-			<a href=""><li>Плазмолифтинг в гинекологии</li></a>
+			<a href="" target="_blank"><li>Криотерапия</li></a>
+			<a href="" target="_blank"><li>Косметический уход</li></a>
+			<a href="" target="_blank"><li>Контурная интимная пластика</li></a>
+			<a href="" target="_blank"><li>Плазмолифтинг в гинекологии</li></a>
 		</ul>
 	</div>
 
