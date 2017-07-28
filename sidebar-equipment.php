c0df3d95cc8e4e5d6dc10c651036addf304469b7
avenirs05 <avenirs05@gmail.com>
2017-07-28 15:49:08 +0300

    last

--- sidebar-equipment.php
+++ sidebar-equipment.php
@@ -1,21 +1,21 @@
 <h2 class="category-header-sidebar">Оборудование</h2>
 <div class="category-content-wrap">
     <div class="category-wrap">
-        <a href="page-equipment-dot_rejuve.php" class="category-name" target="_blank">DOT-омоложение</a>
+        <a href="page-equipment-dot_rejuve.php" class="category-name">DOT-омоложение</a>
     </div>
     <div class="category-wrap">
-        <a href="page-equipment-dot_therapy.php" class="category-name" target="_blank">DOT-терапия</a>
+        <a href="page-equipment-dot_therapy.php" class="category-name">DOT-терапия</a>
     </div>
     <div class="category-wrap">
-        <a href="page-equipment-laser_epilation.php" class="category-name" target="_blank">Лазерная эпиляция</a>
+        <a href="page-equipment-laser_epilation.php" class="category-name">Лазерная эпиляция</a>
     </div>
     <div class="category-wrap">
-        <a href="page-equipment-laser_removal_of_vessels.php" class="category-name" target="_blank">Лазерное удаление сосудов</a>
+        <a href="page-equipment-laser_removal_of_vessels.php" class="category-name">Лазерное удаление сосудов</a>
     </div>
     <div class="category-wrap">
-        <a href="page-equipment-strii.php" class="category-name" target="_blank">Стрии</a>
+        <a href="page-equipment-strii.php" class="category-name">Стрии</a>
     </div>
     <div class="category-wrap">
-        <a href="page-equipment-fractional_rejuve.php" class="category-name" target="_blank">Фракционное омоложение</a>
+        <a href="page-equipment-fractional_rejuve.php" class="category-name">Фракционное омоложение</a>
     </div>   
 </div>
--- sidebar.php
+++ sidebar.php
@@ -1,64 +1,64 @@
 <h2 class="category-header-sidebar">Услуги</h2>
 <div class="category-content-wrap">
     <div class="category-wrap">
-        <a href="" class="category-name">Лазерная косметология</a>
+        <a href="page-services-laser_cosmetology.php" class="category-name">Лазерная косметология</a>
         <ul>
-            <li><a href="" class="category-item">Глубокое лазерное омоложение с повреждением кожи</a></li>
-            <li><a href="" class="category-item">Глубокое лазерное омоложение без повреждения кожи</a></li>
-            <li><a href="" class="category-item">Лечение акне, постакне, розацеа, демодекоза</a></li>
-            <li><a href="" class="category-item">Удаление пигментных пятен</a></li>
-            <li><a href="" class="category-item">Удаление рубцов и растяжек</a></li>
-            <li><a href="" class="category-item">Удаление сосудов на лице и теле</a></li>
-            <li><a href="" class="category-item">Фотоомоложение</a></li>
+            <li><a href="page-services-laser_cosmetology-laser_rejuve_skin_damage.php" class="category-item">Глубокое лазерное омоложение с повреждением кожи</a></li>
+            <li><a href="page-services-laser_cosmetology-laser_rejuve_no_skin_damage.php" class="category-item">Глубокое лазерное омоложение без повреждения кожи</a></li>
+            <li><a href="page-services-laser_cosmetology-acne_treatment.php" class="category-item">Лечение акне, постакне, розацеа, демодекоза</a></li>
+            <li><a href="page-services-laser_cosmetology-remove_pigment_spots.php" class="category-item">Удаление пигментных пятен</a></li>
+            <li><a href="page-services-laser_cosmetology-removing_stretch.php" class="category-item">Удаление рубцов и растяжек</a></li>
+            <li><a href="page-services-laser_cosmetology-remove_vessels_on_face.php" class="category-item">Удаление сосудов на лице и теле</a></li>
+            <li><a href="page-services-laser_cosmetology-photo_rejuve.php" class="category-item">Фотоомоложение</a></li>
         </ul>
     </div>
     
     <div class="category-wrap">
-        <a href="" class="category-name">Коррекция фигуры, целлюлит</a>
+        <a href="page-services-figure_correction.php" class="category-name">Коррекция фигуры, целлюлит</a>
         <ul style="display: none;">        
-            <li><a href="" class="category-item">Комплексные программы по снижению веса</a></li>
-            <li><a href="" class="category-item">Прессотерапия</a></li>
-            <li><a href="" class="category-item">УЗ-липосакция с RF-лифтингом</a></li>
-            <li><a href="" class="category-item">Массаж</a></li>
-            <li><a href="" class="category-item">Иглорефлексотерапия</a></li>
+            <li><a href="page-services-figure_correction-weight_loss.php" class="category-item">Комплексные программы по снижению веса</a></li>
+            <li><a href="page-services-figure_correction-press_therapy.php" class="category-item">Прессотерапия</a></li>
+            <li><a href="page-services-figure_correction-liposuction.php" class="category-item">УЗ-липосакция с RF-лифтингом</a></li>
+            <li><a href="page-services-figure_correction-massage.php" class="category-item">Массаж</a></li>
+            <li><a href="page-services-figure_correction-acupuncture.php" class="category-item">Иглорефлексотерапия</a></li>
         </ul>
     </div>
     
     <div class="category-wrap">
-        <a href="" class="category-name">Инъекционная косметология</a>
+        <a href="page-services-injection_cosmetology.php" class="category-name">Инъекционная косметология</a>
         <ul style="display: none;">        
-            <li><a href="" class="category-item">Биоревитализация</a></li>
-            <li><a href="" class="category-item">Ботокс, Диспорт</a></li>
-            <li><a href="" class="category-item">Изменение формы и объема губ</a></li>
-            <li><a href="" class="category-item">Контурная пластика</a></li>
-            <li><a href="" class="category-item">Мезотерапия</a></li>
-            <li><a href="" class="category-item">Плазмолифтинг</a></li>
+            <li><a href="page-services-injection_cosmetology-biorevitalization.php" class="category-item">Биоревитализация</a></li>
+            <li><a href="page-services-injection_cosmetology-botox_dysport.php" class="category-item">Ботокс, Диспорт</a></li>
+            <li><a href="page-services-injection_cosmetology-change_lips.php" class="category-item">Изменение формы и объема губ</a></li>
+            <li><a href="page-services-injection_cosmetology-contour_plastic.php" class="category-item">Контурная пластика</a></li>
+            <li><a href="page-services-injection_cosmetology-mesotherapy.php" class="category-item">Мезотерапия</a></li>
+            <li><a href="page-services-injection_cosmetology-plazmolifting.php" class="category-item">Плазмолифтинг</a></li>
         </ul>
     </div>
     
     <div class="category-wrap">
-        <a href="" class="category-name">Удаление новообразований кожи</a>
+        <a href="page-services-remove_skin_novelties.php" class="category-name">Удаление новообразований кожи</a>
         <ul style="display: none;">        
-            <li><a href="" class="category-item">Криодеструкция</a></li>
-            <li><a href="" class="category-item">Лазерное удаление</a></li>
+            <li><a href="page-services-remove_skin_novelties-cryodestruction.php" class="category-item">Криодеструкция</a></li>
+            <li><a href="page-services-remove_skin_novelties-laser_removal.php" class="category-item">Лазерное удаление</a></li>
         </ul>
     </div>
     
     <div class="category-wrap">
-        <a href="" class="category-name">Эпиляция</a>
+        <a href="page-services-epilation.php" class="category-name">Эпиляция</a>
         <ul style="display: none;">        
-            <li><a href="" class="category-item">Лазерная эпиляция</a></li>
-            <li><a href="" class="category-item">Фотоэпиляция</a></li>
+            <li><a href="page-services-epilation-laser_epilation.php" class="category-item">Лазерная эпиляция</a></li>
+            <li><a href="page-services-epilation-photo_epilation.php" class="category-item">Фотоэпиляция</a></li>
         </ul>
     </div>
     
     <div class="category-wrap">
-        <a href="" class="category-name">Эстетическая косметология</a>
+        <a href="page-services-esthetic_cosmetology.php" class="category-name">Эстетическая косметология</a>
         <ul style="display: none;">        
-            <li><a href="" class="category-item">Косметический уход</a></li>
-            <li><a href="" class="category-item">Криотерапия</a></li>
-            <li><a href="" class="category-item">Плазмолифтинг в гинекологии и интимной косметологии</a></li>
-            <li><a href="" class="category-item">Контурная интимная пластика</a></li>
+            <li><a href="page-services-esthetic_cosmetology-cosmetic_care.php" class="category-item">Косметический уход</a></li>
+            <li><a href="page-services-esthetic_cosmetology-cryotherapy.php" class="category-item">Криотерапия</a></li>
+            <li><a href="page-services-esthetic-cosmetology-plazmolifting.php" class="category-item">Плазмолифтинг в гинекологии и интимной косметологии</a></li>
+            <li><a href="page-services-esthetic_cosmetology-kontur_intim.php" class="category-item">Контурная интимная пластика</a></li>
         </ul>
     </div>
 </div>
